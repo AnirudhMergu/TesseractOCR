3f29599e784edf1bf1fe48abcfd0a16f42bada81
Anirudh Sai Mergu <AnirudhMergu@users.noreply.github.com>
2018-11-02 14:08:41 +0530

    Update ocr_main.py

--- ocr_main.py
+++ ocr_main.py
@@ -6,7 +6,8 @@ from PIL import Image
 def main():
     # Get File Name from Command Line
     path = input("Enter the file path : ").strip()
-    # load the image
+    
+    # Load the required image
     image = cv2.imread(path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -14,17 +15,17 @@ def main():
     temp = input(
         "Do you want to pre-process the image ?\nThreshold : 1\nGrey : 2\nNone : 0\nEnter your choice : ").strip()
 
-    # If user enter 1, Process Threshold. Else if user enters 2, Process Median Blur. Else, do nothing
+    # If user enters 1, Process Threshold. Else if user enters 2, Process Median Blur. Else, do nothing
     if temp == "1":
         gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
     elif temp == "2":
         gray = cv2.medianBlur(gray, 3)
 
-    # store grayscale image as a temp file to apply OCR
+    # Store grayscale image as a temporary file to apply OCR
     filename = "{}.png".format("temp")
     cv2.imwrite(filename, gray)
 
-    # load the image as a PIL/Pillow image, apply OCR, and then delete the temporary file
+    # Load the image as a PIL/Pillow image, apply OCR, and then delete the temporary file
     text = pytesseract.image_to_string(Image.open(filename))
 
     print("OCR Text is " + text)
